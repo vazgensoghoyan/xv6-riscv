3d7a1ae21d75337b23dcc418a34797f90312e140
vazgensoghoyan <sogvazulchik@gmail.com>
2026-03-05 00:12:18 +0300

    1) removed printf: printing now with write; 2) printing symbol '\n' in cycle

--- pipe_args_linux.c
+++ pipe_args_linux.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wait.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE 8192
 
@@ -40,9 +40,26 @@ int main(int argc, char* argv[]) {
                 exit(EXIT_FAILURE);
             }
 
-            buffer[bytes_read] = '\0';
-            printf("%s", buffer);
+            // if bytes_read == 0 then while is ignored :)
+            // rmk: i was not sure if we can use 'printf'
+            // thats why 'write'
 
+            ssize_t total_written = 0;
+            while (total_written < bytes_read) {
+                ssize_t bytes_written = write(
+                    STDOUT_FILENO,
+                    buffer + total_written,
+                    bytes_read - total_written
+                );
+
+                if (bytes_written < 0) {
+                    fprintf(stderr, "write error occured\n");
+                    close(pipefd[0]);
+                    exit(EXIT_FAILURE);
+                }
+
+                total_written += bytes_written;
+            }
         } while (bytes_read);
 
         close(pipefd[0]);
@@ -75,13 +92,18 @@ int main(int argc, char* argv[]) {
         }
 
         char newline = '\n';
-        ssize_t res = write(pipefd[1], &newline, 1);
+        total_written = 0;
+        
+        do {
+            total_written = write(pipefd[1], &newline, 1); // this can also return 0 :)
 
-        if (res <= 0) {
-            fprintf(stderr, "write error occured\n");
-            close(pipefd[1]);
-            exit(EXIT_FAILURE);
-        }
+            if (total_written < 0) {
+                fprintf(stderr, "write error occured\n");
+                close(pipefd[1]);
+                exit(EXIT_FAILURE);
+            }
+
+        } while (total_written == 0); // we need only 1 bite to be written
     }
 
     close(pipefd[1]);
