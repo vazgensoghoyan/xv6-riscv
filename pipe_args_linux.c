e3da788be9352ec94ab9dc5fe63dc0aa9a969fda
vazgensoghoyan <sogvazulchik@gmail.com>
2026-03-10 01:22:49 +0300

    code refactor + fixed bug with size_t - ssize_t

--- pipe_args_linux.c
+++ pipe_args_linux.c
@@ -6,110 +6,91 @@
 
 #define BUF_SIZE 8192
 
+ssize_t write_all(int fd, const char *buf, size_t len) {
+    ssize_t total = 0;
+    while (total < (ssize_t)len) {
+        ssize_t written = write(fd, buf + total, len - total);
+        if (written < 0)
+            return -1;
+        total += written;
+    }
+    return total;
+}
+
+void safe_close(int fd) {
+    if (close(fd) < 0) {
+        perror("close error");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     int pipefd[2];
-    
+
     if (pipe(pipefd) < 0) {
-        fprintf(stderr, "pipe error occured\n");
+        perror("pipe error");
         exit(EXIT_FAILURE);
     }
 
     pid_t pid = fork();
 
     if (pid < 0) {
-        fprintf(stderr, "fork error occured\n");
-        close(pipefd[0]);
-        close(pipefd[1]);
+        perror("fork error");
+        safe_close(pipefd[0]);
+        safe_close(pipefd[1]);
         exit(EXIT_FAILURE);
     }
 
     if (pid == 0) { // child
 
-        close(pipefd[1]);
+        safe_close(pipefd[1]);
 
         char buffer[BUF_SIZE] = { 0 };
         ssize_t bytes_read;
 
-        do {
-            bytes_read = read(pipefd[0], buffer, sizeof(buffer));
-    
-            if (bytes_read < 0) {
-                fprintf(stderr, "read error occured\n");
-                close(pipefd[0]);
+        while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+            if (write_all(STDOUT_FILENO, buffer, bytes_read) < 0) {
+                perror("write error");
+                safe_close(pipefd[0]);
                 exit(EXIT_FAILURE);
             }
+        }
 
-            // if bytes_read == 0 then while is ignored :)
-            // rmk: i was not sure if we can use 'printf'
-            // thats why 'write'
+        if (bytes_read < 0) {
+            perror("read error");
+            safe_close(pipefd[0]);
+            exit(EXIT_FAILURE);
+        }
 
-            ssize_t total_written = 0;
-            while (total_written < bytes_read) {
-                ssize_t bytes_written = write(
-                    STDOUT_FILENO,
-                    buffer + total_written,
-                    bytes_read - total_written
-                );
-
-                if (bytes_written < 0) {
-                    fprintf(stderr, "write error occured\n");
-                    close(pipefd[0]);
-                    exit(EXIT_FAILURE);
-                }
-
-                total_written += bytes_written;
-            }
-        } while (bytes_read);
-
-        close(pipefd[0]);
-
+        safe_close(pipefd[0]);
         exit(EXIT_SUCCESS);
     }
 
     // parent
 
-    close(pipefd[0]);
+    safe_close(pipefd[0]);
 
     for (int i = 1; i < argc; i++) {
         size_t len = strlen(argv[i]);
-        size_t total_written = 0;
-
-        while (total_written < len) {
-            ssize_t bytes_written = write(
-                pipefd[1],
-                argv[i] + total_written,
-                len - total_written
-            );
-
-            if (bytes_written < 0) {
-                fprintf(stderr, "write error occured\n");
-                close(pipefd[1]);
-                exit(EXIT_FAILURE);
-            }
-
-            total_written += bytes_written;
+        if (write_all(pipefd[1], argv[i], len) < 0) {
+            perror("write error");
+            safe_close(pipefd[1]);
+            exit(EXIT_FAILURE);
         }
 
         char newline = '\n';
-        total_written = 0;
-        
-        do {
-            total_written = write(pipefd[1], &newline, 1); // this can also return 0 :)
-
-            if (total_written < 0) {
-                fprintf(stderr, "write error occured\n");
-                close(pipefd[1]);
-                exit(EXIT_FAILURE);
-            }
-
-        } while (total_written == 0); // we need only 1 bite to be written
+        if (write_all(pipefd[1], &newline, 1) < 0) {
+            perror("write error");
+            safe_close(pipefd[1]);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    close(pipefd[1]);
+    safe_close(pipefd[1]);
 
     if (wait(NULL) < 0) {
-        perror("wait");
+        perror("wait error");
         exit(EXIT_FAILURE);
     }
 
