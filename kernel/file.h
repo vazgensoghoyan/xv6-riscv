87b2282f39426b818ce7e111a0dcdc9ab195a1f1
vazgensoghoyan <sogvazulchik@gmail.com>
2026-03-29 05:48:58 +0300

    minor field added to struct file => relevant small changes in files file.h, file.c, sysfile.c, console.c were made

--- kernel/file.h
+++ kernel/file.h
@@ -7,6 +7,9 @@ struct file {
   struct inode *ip;  // FD_INODE and FD_DEVICE
   uint off;          // FD_INODE
   short major;       // FD_DEVICE
+
+  // added for the task
+  short minor;       // to use minor numbers of io devices
 };
 
 #define major(dev)  ((dev) >> 16 & 0xFFFF)
@@ -31,8 +34,8 @@ struct inode {
 
 // map major device number to device functions.
 struct devsw {
-  int (*read)(int, uint64, int);
-  int (*write)(int, uint64, int);
+  int (*read)(int, int, uint64, int);
+  int (*write)(int, int, uint64, int);
 };
 
 extern struct devsw devsw[];
